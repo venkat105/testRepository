f5ef27f012d224284374681e7e7e55ea4ebce713
venki105 <kancherla.venkat105@gmail.com>
2019-05-21 18:11:21 +0530

    saved the new code in the testclass9.java

--- src/main/java/testRepository/testClass9.java
+++ src/main/java/testRepository/testClass9.java
@@ -12,15 +12,16 @@ for(int j=0;j<10;j++
 }
 
 
-  public void test2() {
-
-	 System.out.println("this is the test2 method");
-}
-  public void test() {
-	  int c=10;
-	  int d=22;
-	  int e=330;
-
+  public void test_Sample()
+  {
+	  for(int j=0;j<10;j++
+				) {
+			System.out.println("this is for loop");
+		} 
+	  int a11=50;
+	  int b32=90;
+	  int c21=50;
+	  int d42=90;
   }
 
 }
