97fd3b190cbdaa2f0592cd8f424e3a9ee3e4416f
Venkateswarlu Kancherla <venkanch@publicisgroupe.net>
2019-05-21 15:13:21 +0530

    adding latest file

--- src/main/java/testRepository/testClass9.java
+++ src/main/java/testRepository/testClass9.java
@@ -1,5 +1,12 @@
 package testRepository;
 
 public class testClass9 {
-
+int a=50;
+int b=90;
+public static void main(String args[]) {
+for(int j=0;j<10;j++
+		) {
+	System.out.println("this is for loop");
+}
+}
 }
